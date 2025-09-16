fdc4ce08a1351f55571931df11bbd02d110c851b
fvilpaz <fvilpaz@fvilpaz-Thinkpad>
2025-09-16 14:18:09 +0200

    FEAT: add_to_start & swap_first_two functions

--- listas.c
+++ listas.c
@@ -18,7 +18,7 @@ t_node  *create_node(int num)
         return(NULL);
     // - Asigna el número
     result->number =  num;
-    // - Pone 'next' a NULL
+    // - Pon 'next' a NULL
     result->next = NULL;
     // - Devuelve el puntero al nodo
     return (result);
@@ -38,7 +38,7 @@ void    add_to_end(t_node **head, int n)
     if (*head == NULL)
     {
         *head = new_node;  // La cabeza ahora apunta al nuevo nodo
-        return;
+        return ;
     }
 
     // 3. Caso: Lista NO vacía
@@ -52,6 +52,32 @@ void    add_to_end(t_node **head, int n)
 
     // 4. Enlazamos el nuevo nodo al final
     current->next = new_node;
+}
+
+/* función para añadir nodo al principio, igual que antes, doble puntero 
+para modificar head y le pasmos el entero del nodo que vamos a crear*/
+void	add_to_start(t_node **head, int n)
+{
+	t_node *new_node = create_node(n);
+	if (!new_node) // si falla new_node salimos y devuelve el head que habia
+		return ;
+	new_node->next = *head; // lo que le siga al nodo apunt a head
+	*head = new_node; // ahora head apunta al nuevo nodo
+}
+
+// función para intercambiar los dos primeros nodos
+// Esta vez sol usamos doble puntero a head porque solo habra modificacion en head
+void	swap_first_two(t_node **head)
+{
+	// si el nodo esta vacio o si solo hay un nodo, salimos
+	if (*head == NULL || (*head)->next == NULL)
+		return ;
+	t_node *first = *head; // el primer nodo apunta a la cabeza
+	t_node *second = first->next; // el segundo nodo es el que le sigue al primero
+
+	first->next = second->next; // cambiamos y ahora el primer nodo apunta al lo que apuntaba el 2
+	second->next = first; // el segundo apuntaba a lo mismo que el primero con el cambio, ahora apunta al primero
+	*head = second; // ahora head apunta al 2 nodo (que seria el primero)
 }
 
 // función para imprimir la lista
@@ -75,7 +101,7 @@ void	free_list(t_node *head)
 	{
 		tmp = current; // guardamos current en tmp
 		current = current->next;	// avanzamos la posicion
-		free(tmp); // liberamos la memria
+		free(tmp); // liberamos la memoria
 	}
 }
 // 4. Función principal
@@ -86,9 +112,15 @@ int main(void)
     add_to_end(&head, 42);
     add_to_end(&head, 21);
     add_to_end(&head, 10);
+	add_to_start(&head, 1);
 
     // Imprimimos la lista
     t_node  *current = head;
+	printf("Lista antes de swap: ");
+	print_list(head);
+
+	swap_first_two(&head);
+	printf("Lista después del swap: ");
 	print_list(head);
     
     // Liberar memoria
